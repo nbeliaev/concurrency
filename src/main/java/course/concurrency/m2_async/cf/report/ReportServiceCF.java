2c89b148d702a6bf236f764a2bf943f80702d9d9
Nick <fr13.dev@gmail.com>
2023-06-14 17:02:11 +0300

    study fork join pool

--- src/main/java/course/concurrency/m2_async/cf/report/ReportServiceCF.java
+++ src/main/java/course/concurrency/m2_async/cf/report/ReportServiceCF.java
@@ -6,11 +6,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 
 public class ReportServiceCF {
 
-    private ExecutorService executor = ForkJoinPool.commonPool();
+    //private ExecutorService executor = ForkJoinPool.commonPool();
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
     private LoadGenerator loadGenerator = new LoadGenerator();
 
