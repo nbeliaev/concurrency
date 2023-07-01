025cc62c9bdbce1712298a3ed6640029b14a697d
Nick <fr13.dev@gmail.com>
2023-07-01 23:11:53 +0300

    add pessimistic impl

--- src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
@@ -1,16 +1,14 @@
 package course.concurrency.exams.auction;
 
 public class AuctionPessimistic implements Auction {
-
-    private Notifier notifier;
+    private final Notifier notifier;
+    private volatile Bid latestBid = new Bid(Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE);
 
     public AuctionPessimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
-
-    public boolean propose(Bid bid) {
+    public synchronized boolean propose(Bid bid) {
         if (bid.getPrice() > latestBid.getPrice()) {
             notifier.sendOutdatedMessage(latestBid);
             latestBid = bid;
